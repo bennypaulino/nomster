ef5688b8c2639bdcd4b1793a5d029a028bc0c502
Benny Paulino <santiagopro71@gmail.com>
2016-03-05 02:31:21 +0000

    Adjust places_controller.rb so that only logged-in users can create places, add another column to the database that will store the current_user's id together with the place they create, adjust the models for user.rb and places.rb to associate users with places, adjust places_controller.rb, adjust places/index.html.erb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,17 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @places = Place.all.paginate(:page => params[:page], :per_page => 5)
   end
+
 
   def new
     @place = Place.new
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
